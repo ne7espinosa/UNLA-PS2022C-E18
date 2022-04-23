099142690a500bf85cc064e2281636d2c70d46f9
Nicolás Espinosa <ne_espinosa@hotmail.com>
2022-04-23 00:10:58 -0300

    Se muestra imagen y texto de McDonalds en la primer tab
    -Se agregó imagen mcdonaldsicon.png en assets
    -Se agregó texto y imagen de McDonalds”

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -8,6 +8,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <Image source={require('../assets/images/mcdonaldsicon.png')} style={styles.logo}/>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
@@ -29,4 +30,9 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  logo: {
+    marginTop: 20,
+    width: 66,
+    height: 60
+  }
 });
