8fe872a16d2ca7aaae5dd4a889971b36e27be57c
Sofia Sanchez Antelo <sofia.sanchez@claro.com.ar>
2022-06-02 13:56:07 -0300

    Cambios en la vista detalle pedido

--- screens/PedidoScreen.tsx
+++ screens/PedidoScreen.tsx
@@ -5,7 +5,7 @@ import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { PedidoContext } from '../contexts/pedidoContext';
 import { RootStackParamList, RootStackScreenProps } from '../types';
-import { AntDesign,MaterialIcons,Ionicons } from '@expo/vector-icons';
+import { AntDesign,MaterialIcons,Ionicons,FontAwesome  } from '@expo/vector-icons';
 
 
 
@@ -19,34 +19,46 @@ export default function PedidoScreen(props: PedidoProps) {
   const [precioTotal, setPrecioTotal] = useState<number>(0);
 
   return (
+
     <View style={styles.container}>
       <Text style={styles.title}>Detalle del pedido</Text>
+
       {
       pedidoContext.productos.length > 0 ?
-          <View>
-            <Text style={styles.precioTotal}>Resumen</Text>
+        <View style={styles.container}>
 
-            <Text style={styles.buttonSubtotal}>Subtotal: ${productos.reduce((sumar, item) => sumar + item.precioTotal, 0)} </Text>      
-               <Text style={styles.pagoTarjeta}> <Ionicons name="card-outline" size={24} color="black" />Pago con tarjeta</Text>                  
-              </View>     
+      <Text style={styles.subtitulo}>Resumen</Text>
+
+            <Text style={ styles.subtotal}>Subtotal: ${productos.reduce((sumar, item) => sumar + item.precioTotal, 0)} </Text>
+            <Text style={styles.subtitulo}>Método de pago</Text>      
+            <Text style={styles.pagoTarjeta}> <Ionicons name="card-outline" size={24} color="white" />Pago con tarjeta <AntDesign name="checkcircle" size={24} color={'#F2A30F'} /></Text>                  
+            
+              </View>    
+            
+            
           :
-
-          <View></View>
-
+        
+          <View>
+        
+          </View>
         }
-      
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/PedidoScreen.tsx" />
 
-      <Text style={styles.precioTotal}>Total: ${productos.reduce((sumar, item) => sumar + item.precioTotal, 0)} </Text>
+            <View style={styles.buttonConfirmar}>
+            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
 
+            <EditScreenInfo path="/screens/PedidoScreen.tsx" />
 
-      <View style={styles.buttonConfirmar}>
+            <Text style={styles.subtitulo}>Total: ${productos.reduce((sumar, item) => sumar + item.precioTotal, 0)} </Text>
+
                 <Button color={'#F2A30F'} title='Pagar' onPress={() =>{}}></Button>
                 </View>
 
+      </View>
+    
+   
+     
+
       
-    </View>
   );
 }
 
@@ -56,79 +68,41 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  boxPrincipal: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    padding: 10,
-  },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-  },
-  advertencia: {
-    fontSize: 15,
-    textAlign: 'center'
-  },
-  nombre: {
-    fontSize: 10,
-    marginVertical: 30,
-  },
+    },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: '100%',
+    
   },
-  muestra: {
-    marginTop: 20,
-    width: 50,
-    height: 50,
-    alignContent: 'flex-start',
-    borderRadius: 25,
+  separator2: {
+    marginVertical: 20,
+    height: 1,
+    width: '30%',
+    
   },
-  botones: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    marginTop: 40
-  },
-  cantidad: {
-    fontSize: 12,
-    right: 20,
-    left: 1
-  },
-  botonesFinal: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-  },
-  buttonContenedorSP: {
-    width: 135,
-    height: 35
-  },
-  buttonSeguirPidiendo: {
-    backgroundColor: '#ffffff',
-    width: 135,
-    height: 35
-  },
-  buttonSeguirPidiendoText:
-  {
-    marginTop: 7,
+  subtotal:{
+    fontSize: 20,
+    marginBottom: 5,
     textAlign: 'center',
-    color: 'black',
+    marginTop: 50,
   },
-  precioTotal:
-  {
+  
+  subtitulo:{
     fontSize: 20,
     marginBottom: 5,
     textAlign: 'center',
     fontWeight: 'bold',
   },
 
-    pagoTarjeta:
-  {
+  pagoTarjeta:{
     fontSize: 15,
     marginBottom: 5,
-    textAlign: 'right',
     fontWeight: 'bold',
+    marginTop: 50,
   },
 
   
@@ -138,12 +112,4 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 10,
 },
-
-buttonSubtotal:
-{
-    marginTop: 7,
-    textAlign: 'center',
-    color: 'black',
-  },
-
 });
